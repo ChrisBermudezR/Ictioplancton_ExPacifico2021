b7ff43ec976004f12ddf68d714a82f8ba23add04
Christian Bermúdez-Rivas <41581329+ChrisBermudezR@users.noreply.github.com>
2022-11-25 12:02:10 -0500

    Adición de los anaĺisis biológicos

--- 03_An_Expl_DatosBiologicos/Inext_Analisis_Biologico.R
+++ 03_An_Expl_DatosBiologicos/Inext_Analisis_Biologico.R
@@ -13,48 +13,141 @@ library(vegan)
 library(dplyr)
 library(tidyverse)
 library(tidyr)
+library(gridExtra)
 
 data(BCI)
-datosDiv <- BCI
+DATA <- BCI
 
-Codigo_fito<-read.table("./Biologicos/DatosP_Fitoplancton/Matriz_Codigos_Fito2.csv", sep=",", header = TRUE,)
 
-#Transectos
+Codigo_fito<-read.table("./Biologicos/DatosP_Fitoplancton/Matriz_Incidencia_Fito.csv", sep=",", header = TRUE,)
 
-AmaralesAlta<-filter(Codigo_fito, Transecto=="Amarales" | Marea =="Alta")
-SanquiangaAlta<-filter(Codigo_fito, Transecto=="Sanquianga"| Marea =="Alta")
-GuascamaAlta<-filter(Codigo_fito, Transecto=="Guascama"| Marea =="Alta")
+#Transectos####
+
+Amarales<-filter(Codigo_fito, Transecto=="Amarales")
+Sanquianga<-filter(Codigo_fito, Transecto=="Sanquianga")
+Guascama<-filter(Codigo_fito, Transecto=="Guascama")
+
+amaSum<-as.data.frame(t(Amarales[,6:191]%>% summarise_all(sum)))
+sanSum<-as.data.frame(t(Sanquianga[,6:191]%>% summarise_all(sum)))
+guaSum<-as.data.frame(t(Guascama[,6:191]%>% summarise_all(sum)))
+
+amaDiv<-filter(amaSum, V1>0)
+sanDiv<-filter(sanSum, V1>0)
+guaDiv<-filter(guaSum, V1>0)
+
+amaVec<-as_vector(amaDiv$V1)
+sanVec<-as_vector(sanDiv$V1)
+guaVec<-as_vector(guaDiv$V1)
+
+amaVec<-append(amaVec,12,0)
+sanVec<-append(sanVec,12,0)
+guaVec<-append(guaVec,12,0)
+
+Transectos<-list(amaVec, sanVec, guaVec)
+names(Transectos)<-c("Amarales", "Sanquianga", "Guascama")
+
+#Inext###
+
+# Comando general de iNEXT (calcula muchas cosas)
+Transectos_Plot <- iNEXT(Transectos, 
+                   q=c(0,1,2), 
+                   datatype = "incidence_freq",
+                   endpoint = 20) # q = 0 es la riqueza (diversidades verdaderas)
+
+# Sample-size-based R/E curves, separating plots by "site"
+
+png(filename="Transectos_Plot_hill_Plot_Assamblage.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Transectos_Plot, type=1,facet.var = "Assemblage")
+dev.off()
+
+png(filename="Transectos_Plot_coverage_Plot_Assamblage.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Transectos_Plot, type=3,facet.var = "Assemblage")
+dev.off()
+
+
+
+png(filename="Transectos_Plot_hill_Plot_Order.q.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Transectos_Plot, type=1,facet.var = "Order.q")
+dev.off()
+
+png(filename="Transectos_Plot_hill_Plot_both.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Transectos_Plot, type=1,facet.var = "Both")
+dev.off()
+
+
+
+
+
+#Transectos + Mareas ####
+
+AmaralesAlta<-filter(Codigo_fito, Transecto=="Amarales" & Marea =="Alta")
+SanquiangaAlta<-filter(Codigo_fito, Transecto=="Sanquianga" & Marea =="Alta")
+GuascamaAlta<-filter(Codigo_fito, Transecto=="Guascama" & Marea =="Alta")
+
+amaSumAlta<-as.data.frame(t(AmaralesAlta[,6:191]%>% summarise_all(sum)))
+sanSumAlta<-as.data.frame(t(SanquiangaAlta[,6:191]%>% summarise_all(sum)))
+guaSumAlta<-as.data.frame(t(GuascamaAlta[,6:191]%>% summarise_all(sum)))
+
+
+
+amaAltaDiv<-filter(amaSumAlta, V1>0)
+sanAltaDiv<-filter(sanSumAlta, V1>0)
+guaAltaDiv<-filter(guaSumAlta, V1>0)
+
+amaVecAlta<-as_vector(amaAltaDiv$V1)
+sanVecAlta<-as_vector(sanAltaDiv$V1)
+guaVecAlta<-as_vector(guaAltaDiv$V1)
+
+amaVecAlta<-append(amaVecAlta,6,0)
+sanVecAlta<-append(sanVecAlta,6,0)
+guaVecAlta<-append(guaVecAlta,6,0)
 
 #Bajas
 
-AmaralesBaja<-filter(Codigo_fito, Transecto=="Amarales" | Marea =="Baja")
-SanquiangaBaja<-filter(Codigo_fito, Transecto=="Sanquianga"| Marea =="Baja")
-GuascamaBaja<-filter(Codigo_fito, Transecto=="Guascama"| Marea =="Baja")
+AmaralesBaja<-filter(Codigo_fito, Transecto=="Amarales" & Marea =="Baja")
+SanquiangaBaja<-filter(Codigo_fito, Transecto=="Sanquianga" & Marea =="Baja")
+GuascamaBaja<-filter(Codigo_fito, Transecto=="Guascama" & Marea =="Baja")
 
-amaSumAlta<-as_vector(AmaralesAlta[,5:193]%>% summarise_all(sum))
-sanSumAlta<-as_vector(SanquiangaAlta[,5:193]%>% summarise_all(sum))
-guaSumAlta<-as_vector(GuascamaAlta[,5:193]%>% summarise_all(sum))
+amaSumBaja<-as.data.frame(t(AmaralesBaja[,6:191]%>% summarise_all(sum)))
+sanSumBaja<-as.data.frame(t(SanquiangaBaja[,6:191]%>% summarise_all(sum)))
+guaSumBaja<-as.data.frame(t(GuascamaBaja[,6:191]%>% summarise_all(sum)))
 
-amaSumBaja<-as_vector(AmaralesBaja[,5:193]%>% summarise_all(sum))
-sanSumBaja<-as_vector(SanquiangaBaja[,5:193]%>% summarise_all(sum))
-guaSumBaja<-as_vector(GuascamaBaja[,5:193]%>% summarise_all(sum))
+amaBajaDiv<-filter(amaSumBaja, V1>0)
+sanBajaDiv<-filter(sanSumBaja, V1>0)
+guaBajaDiv<-filter(guaSumBaja, V1>0)
 
-Transectos<-list(amaSumAlta, sanSumAlta, guaSumAlta, amaSumBaja, sanSumBaja, guaSumBaja)
+amaVecBaja<-as_vector(amaBajaDiv$V1)
+sanVecBaja<-as_vector(sanBajaDiv$V1)
+guaVecBaja<-as_vector(guaBajaDiv$V1)
+
+amaVecBaja<-append(amaVecBaja,6,0)
+sanVecBaja<-append(sanVecBaja,6,0)
+guaVecBaja<-append(guaVecBaja,6,0)
+
+Transectos<-list(amaVecAlta, sanVecAlta, guaVecAlta, amaVecBaja, sanVecBaja, guaVecBaja)
 names(Transectos)<-c("Ama_Alta", "San_Alta","Gua_Alta", "Ama_Baja", "San_Baja","Gua_Baja")
 
 #Inext####
 
-
-
 # Comando general de iNEXT (calcula muchas cosas)
 est.Comms <- iNEXT(Transectos, 
-                   q=c(0, 1, 2), 
-                   datatype = "abundance",
-                   endpoint=5000) # q = 0 es la riqueza (diversidades verdaderas)
+                 q=c(0,1,2), 
+                 datatype = "incidence_freq",
+                 endpoint = 20) # q = 0 es la riqueza (diversidades verdaderas)
 
 # Sample-size-based R/E curves, separating plots by "site"
+
+png(filename="hill_Plot_Assamblage.png", height =1000 , width = 1500, units = "px")
 ggiNEXT(est.Comms, type=1,facet.var = "Assemblage")
+dev.off()
+
+png(filename="hill_Plot_Order.q.png", height =1000 , width = 1500, units = "px")
 ggiNEXT(est.Comms, type=1,facet.var = "Order.q")
+dev.off()
+
+png(filename="hill_Plot_both.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(est.Comms, type=1,facet.var = "Both")
+dev.off()
 
 
 
@@ -63,194 +156,137 @@ ggiNEXT(est.Comms, type=1,facet.var = "Order.q")
 
 
 
+#Marea
+
+Alta<-filter(Codigo_fito, Marea=="Alta")
+Baja<-filter(Codigo_fito, Marea=="Baja")
 
 
-# Cobertura de muestra C1
-SC.C1 <- est.Comms$iNextEst$size_based[which(est.Comms$iNextEst$size_based$Method == "observed"), 7]
-
-# Cobertura de muestra C2
-SC.C2 <- est.Comms$iNextEst$coverage_based[which(est.Comms$iNextEst$coverage_based$Method == "observed"), 7]
-
-# Añadimos las coberturas a una tabla
-coverage <- cbind(C1 = SC.C1, C2 = SC.C2) 
+AltaSum<-as.data.frame(t(Alta[,6:191]%>% summarise_all(sum)))
+BajaSum<-as.data.frame(t(Baja[,6:191]%>% summarise_all(sum)))
 
 
-
-# Abundancias totales por comunidad
-abundances <- colSums(datosDiv.t)
-
-# Valor máximo para extrapolar (no más del doble de las abundancias)
-max.Extrapol <- abundances * 2
-
-#Estimadores para cada comunidad####
-rare.C1 <- iNEXT(datosDiv.t[, 1], q = 0, datatype = "abundance", knots = max.Extrapol[1])
-rare.C2 <- iNEXT(datosDiv.t[, 2], q = 0, datatype = "abundance", knots = max.Extrapol[2])
+AltaDiv<-filter(AltaSum, V1>0)
+BajaDiv<-filter(BajaSum, V1>0)
 
 
-
-# Filtro para extraer los valores de ambas comunidades al 65% de cobertura (redondeado a dos decimales)
-S.C1.rare <- dplyr::filter(rare.C1$iNextEst, round(SC, 2) == 0.65)$qD
-S.C2.rare <- dplyr::filter(rare.C2$iNextEst, round(SC, 2) == 0.65)$qD
-
-# Añadimos los valores promediados a una tabla
-S.Comms.rare <- cbind(C1 = mean(S.C1.rare), C2 = mean(S.C2.rare))
+AltaVec<-as_vector(AltaDiv$V1)
+BajaVec<-as_vector(BajaDiv$V1)
 
 
+AltaVec<-append(AltaVec,18,0)
+BajaVec<-append(BajaVec,18,0)
 
 
-# Filtro para separar los datos interpolados y extrapolados de ambas comunidades
-# (abundancia menor o igual a la observada = interpolación, mayor = extrapolación)
-C1.inter <- dplyr::filter(rare.C1$iNextEst, m <= abundances[1])
-C1.extra <- dplyr::filter(rare.C1$iNextEst, m > abundances[1])
-C2.inter <- dplyr::filter(rare.C2$iNextEst, m <= abundances[2])
-par(mfrow = c(1, 1), mar = c(4, 4, 1, 2), oma = c(0, 2, 0, 0))
+Marea<-list(AltaVec, BajaVec)
+names(Marea)<-c("Alta", "Baja")
 
-# Lienzo vacío para la grafica (con los limites de X e Y según los datos)
-plot(NA, # Lienzo sin datos
-     xlab = "Cobertura de muestra", # Etiqueta del eje X
-     ylab = "Riqueza estimada", # Etiqueta del eje Y
-     xlim = c(0, 1), # Límites del eje X (según los datos)
-     ylim = c(0, 200)) # Límites del eje Y (según los datos)
+#Inext####
 
-# Polígono con los intervalos de confianza para la Comunidad 1
-polygon(c(rare.C1$iNextEst$SC , rev(rare.C1$iNextEst$SC)), # Valores X del intervalo
-        c(rare.C1$iNextEst$qD.LCL, rev(rare.C1$iNextEst$qD.UCL)), # Valores Y del intervalo
-        col = rgb(222/255, 122/255, 122/255, 0.6), # Color (en RGB) y transparencia del polígono
-        border = NA) # No mostrar el borde del polígono
+# Comando general de iNEXT (calcula muchas cosas)
+Marea_Plot <- iNEXT(Marea, 
+                         q=c(0,1,2), 
+                         datatype = "incidence_freq",
+                         endpoint = 20) # q = 0 es la riqueza (diversidades verdaderas)
 
-# Polígono con los intervalos de confianza para la Comunidad 2
-polygon(c(rare.C2$iNextEst$SC , rev(rare.C2$iNextEst$SC)), # Valores X del intervalo
-        c(rare.C2$iNextEst$qD.LCL, rev(rare.C2$iNextEst$qD.UCL)), # Valores Y del intervalo
-        col = rgb(122/255, 170/255, 222/255, 0.6), # Color (en RGB) y transparencia del polígono
-        border = NA) # No mostrar el borde del polígono
+# Sample-size-based R/E curves, separating plots by "site"
 
-# Curva de acumulación basada en cobertura para la Comunidad 1 (valores interpolados)
-lines(rare.C1$iNextEst[which(rare.C1$iNextEst$m <= abundances[1]), 7], # Valores de X
-      rare.C1$iNextEst[which(rare.C1$iNextEst$m <= abundances[1]), 4], # Valores de Y
-      type = "l", # Tipo de línea (sólida)
-      col = "red") # Color de la línea
+png(filename="Marea_Plot_hill_Plot_Assamblage.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Marea_Plot, type=1,facet.var = "Assemblage")
+dev.off()
 
-# Curva de acumulación basada en cobertura para la Comunidad 1 (valores extrapolados)
-lines(rare.C1$iNextEst[which(rare.C1$iNextEst$m > abundances[1]), 7], # Valores de X
-      rare.C1$iNextEst[which(rare.C1$iNextEst$m > abundances[1]), 4], # Valores de Y
-      lty = 2, # Tipo de línea (punteada)
-      col = "red") # Color de la línea
-
-# Curva de acumulación basada en cobertura para la Comunidad 2 (valores interpolados)
-lines(rare.C2$iNextEst[which(rare.C2$iNextEst$m <= abundances[2]), 7], # Valores de X
-      rare.C2$iNextEst[which(rare.C2$iNextEst$m <= abundances[2]), 4], # Valores de Y
-      type = "l", # Tipo de línea (sólida)
-      col = "blue") # Color de la línea
-
-# Curva de acumulación basada en cobertura para la Comunidad 2 (valores extrapolados)
-lines(rare.C2$iNextEst[which(rare.C2$iNextEst$m > abundances[2]), 7], # Valores de X
-      rare.C2$iNextEst[which(rare.C2$iNextEst$m > abundances[2]), 4], # Valores de Y
-      lty = 2, # Tipo de línea (punteada)
-      col = "blue") # Color de la línea
-
-# Línea punteada en el valor de cobertura mínima común 
-abline(v = min(coverage), lty = 2)
-
-# Leyenda de la grafica
-legend(x = "bottomright", # Posición
-       legend = c("C1", "C2"), # Texto de la leyenda
-       lty = c(1, 1), # Tipos de línea de los símbolos
-       col = c("red", "blue"), # Colores de los símbolos
-       lwd = 2, # Grosor de las líneas de los símbolos
-       inset = c(0.025, 0.05)) # Márgenes de la leyenda
+png(filename="Marea_Plot_coverage_Plot_Assamblage.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Marea_Plot, type=3,facet.var = "Assemblage")
+dev.off()
 
 
 
-# Vector para almacenar el número de especies por unidad de muestreo
-S <- c()
+png(filename="Marea_Plot_hill_Plot_Order.q.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Marea_Plot, type=1,facet.var = "Order.q")
+dev.off()
 
-# Ciclo para recorrer la matriz de datos
-for(i in 1:nrow(datosDiv)){ # Ciclo desde uno hasta el total de filas de los datos (comunidades)
-  S.tmp <- length(which(datosDiv[i, ] > 0)) # Número de especies (columnas) con abundancia > 0 por unidad de muestreo
-  S <- append(S, S.tmp) # Añadimos el número de especies del sitio i al vector de especies
-}
-
-# Índice de Gini-Simpson
-Simpson <- diversity(datosDiv, index  = "simpson")
-
-# Índice de Shannon
-Shannon <- diversity(datosDiv, index = "shannon")
-
-# Índice de Pielou
-Pielou <- Shannon / log(S) 
-
-# Combinamos los índices en una tabla
-indices <- cbind(S = S, Simpson = Simpson, Shannon = Shannon, Pielou = Pielou)
-
-# Transformamos los índices tradicionales en índices de diversidad verdadera
-indices <- dplyr::mutate(as.datosDiv.frame(indices),
-                         q0 = S, # Riqueza (q = 0)
-                         q1 = exp(Shannon), # Exponencial de Shannon (q = 1)
-                         q2 = 1 / (1 - Simpson)) # Inverso de Simpson (q = 2)
+png(filename="Marea_Plot_hill_Plot_both.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Marea_Plot, type=1,facet.var = "Both")
+dev.off()
 
 
 
 
-# Vector para almacenar el número de especies por unidad de muestreo
-indices <- renyi(datosDiv, # Matriz de datos
-                 scales = c(0, 1, 2), # Ordenes de diversidad
-                 hill = TRUE) # Números de Hill (cuando es FALSE calcula entropías de Renyi)
-
-# Nombre de las columnas de la tabla de índices
-colnames(indices) <- c("q0", "q1", "q2")
 
 
-# Tamaño (número de individuos) de cada comunidad a la misma cobertura de muestra
-# (los valores de cobertura los tomamos de la variable coverage previamente calculada)
-size.C1 <- Coverage2Size(datosDiv.t[, 1], SampleCoverage = min(coverage))
-size.C2 <- Coverage2Size(datosDiv.t[, 2], SampleCoverage = min(coverage))
+#Sector
 
-# Estimados de diversidad para cada comunidad a la misma cobertura de muestra
-# (iNEXT no permite el calculo de un único tamaño, así que ponemos un cero que luego filtraremos)
-C1.DivProf <- iNEXT(datosDiv.t[, 1], q = seq(0, 2, by = 0.2), size = c(0, size.C1))$iNextEst
-C2.DivProf <- iNEXT(datosDiv.t[, 2], q = seq(0, 2, by = 0.2), size = c(0, size.C2))$iNextEst
+Oceanico<-filter(Codigo_fito, Sector=="Oceanico")
+Costero<-filter(Codigo_fito, Sector=="Costero")
 
-# Filtro para eliminar los valores de cero que creamos en el paso anterior
-C1.DivProf <- dplyr::filter(C1.DivProf, m > 0)
-C2.DivProf <- dplyr::filter(C2.DivProf, m > 0)
 
-# Lienzo vacío para el perfil de diversidad
-plot(NA, # Lienzo sin datos
-     xlab = "Órdenes", # Etiqueta del eje X
-     ylab = "ENS", # Etiqueta del eje Y
-     xlim = c(0, 2), # Límites del eje X (según los datos)
-     ylim = c(0, 120)) # Límites del eje Y (según los datos)
+OceanicoSum<-as.data.frame(t(Oceanico[,6:191]%>% summarise_all(sum)))
+CosteroSum<-as.data.frame(t(Costero[,6:191]%>% summarise_all(sum)))
 
-# Perfil de la Comunidad 1
-lines(x = C1.DivProf$order, # Valores de X
-      y = C1.DivProf$qD, # Valores de Y
-      type = "l", # Tipo de línea (sólida)
-      col = "red") # Color de la línea
 
-# Perfil de la Comunidad 2
-lines(C2.DivProf$order, # Valores de X
-      C2.DivProf$qD, # Valores de Y
-      type = "l", # Tipo de línea (sólida)
-      col = "blue") # Color de la línea
+OceanicoDiv<-filter(OceanicoSum, V1>0)
+CosteroDiv<-filter(CosteroSum, V1>0)
 
-# Puntos de la Comunidad 1
-points(x = c(0, 1, 2), # Valores de X
-       y = c(31.255, 24.994, 19.648), # Valores de Y
-       pch = 19, # Forma del símbolo (19 = círculo)
-       col = "red") # Color del símbolo
 
-# Puntos de la Comunidad 2
-points(x = c(0, 1, 2), # Valores de X
-       y = c(112, 58.277, 22.549), # Valores de Y
-       pch = 19, # Forma del símbolo (19 = círculo)
-       col = "blue") # Color del símbolo
+OceanicoVec<-as_vector(OceanicoDiv$V1)
+CosteroVec<-as_vector(CosteroDiv$V1)
 
-legend(x = "topright", # Posición
-       legend = c("C1 (65%)", "C2 (65%)"), # Texto de la leyenda
-       lty = c(1, 1), # Tipos de línea de los símbolos
-       col = c("red", "blue"), # Colores de los símbolos
-       lwd = 2, # Grosor de las líneas de los símbolos
-       inset = c(0.025, 0.05)) # Márgenes de la leyenda
+
+OceanicoVec<-append(OceanicoVec,18,0)
+CosteroVec<-append(CosteroVec,18,0)
+
+
+Sector<-list(OceanicoVec, CosteroVec)
+names(Sector)<-c("Oceanico", "Costero")
+
+#Inext####
+
+# Comando general de iNEXT (calcula muchas cosas)
+Sector_Plot <- iNEXT(Sector, 
+                    q=c(0,1,2), 
+                    datatype = "incidence_freq",
+                    endpoint = 20) # q = 0 es la riqueza (diversidades verdaderas)
+
+# Sample-size-based R/E curves, separating plots by "site"
+
+png(filename="Sector_Plot_hill_Plot_Assamblage.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Sector_Plot, type=1,facet.var = "Assemblage")
+dev.off()
+
+png(filename="Sector_Plot_coverage_Plot_Assamblage.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Sector_Plot, type=3,facet.var = "Assemblage")
+dev.off()
 
 
 
+png(filename="Sector_Plot_hill_Plot_Order.q.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Sector_Plot, type=1,facet.var = "Order.q")
+dev.off()
+
+png(filename="Sector_Plot_hill_Plot_both.png", height =1000 , width = 1500, units = "px")
+ggiNEXT(Sector_Plot, type=1,facet.var = "Both")
+dev.off()
+
+
+
+Transectos_Plot_Hill<-ggiNEXT(Transectos_Plot, type=1,facet.var = "Assemblage")
+Marea_Plot_Hill<-ggiNEXT(Marea_Plot, type=1,facet.var = "Assemblage")
+Sector_Plot_Hill<-ggiNEXT(Sector_Plot, type=1,facet.var = "Assemblage")
+
+Transectos_Plot_Coverage<-ggiNEXT(Transectos_Plot, type=3,facet.var = "Assemblage")
+Marea_Plot_Coverage<-ggiNEXT(Marea_Plot, type=3,facet.var = "Assemblage")
+Sector_Plot_Coverage<-ggiNEXT(Sector_Plot, type=3,facet.var = "Assemblage")
+
+
+png(filename="Total_Plot_hill_Plot_Assamblage.png", height =1500 , width = 1000, units = "px")
+gridExtra:: grid.arrange(Transectos_Plot_Hill,Marea_Plot_Hill,Sector_Plot_Hill, ncol=1)
+dev.off()
+
+
+
+png(filename="Total_Plot_coverage_Plot_Assamblage.png", height =1500 , width = 1000, units = "px")
+gridExtra:: grid.arrange(Transectos_Plot_Coverage,Marea_Plot_Coverage,Sector_Plot_Coverage, ncol=1)
+dev.off()
+
+
+#
